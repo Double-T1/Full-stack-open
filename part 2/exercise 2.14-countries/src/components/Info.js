1092fec9655fea62ed8938329575380cfcc2ecaa
CWillC <weichen199603@gmail.com>
2022-02-24 23:12:09 +0800

    finally finished 2c

--- part 2/exercise 2.14-countries/src/components/Info.js
+++ part 2/exercise 2.14-countries/src/components/Info.js
@@ -2,17 +2,19 @@ import Country from "./Country"
   
 const MaxTenContries = ({countriesToShow, setToShow}) => {
     return countriesToShow.map(ele => {
-    return (
-        <li key={ele.name.common}>
-            {ele.name.common} <button type='submit' onClick={() => setToShow([ele])}>show</button>
-        </li>
-    )
+        return (
+            <li key={ele.name.common}>
+                {ele.name.common} <button type='submit' onClick={() => setToShow([ele])}>show</button>
+            </li>
+        )
     })
 }
   
 const Info = ({countriesToShow, setToShow}) => {
     let len = countriesToShow.length
-    if (len===1) {
+    if (len===0) {
+        return <p>No country</p>
+    } else if (len===1) {
         return <Country country={countriesToShow[0]}/>
     } else if (len < 10) {
         return <MaxTenContries countriesToShow={countriesToShow} setToShow={setToShow}/>
